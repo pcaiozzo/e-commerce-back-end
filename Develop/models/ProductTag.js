b1f64048591ca0f7472a83db9c9e796e090d943c
pcaiozzo <paigecaiozzo@gmail.com>
2022-07-01 23:20:05 -0400

    added requirements for models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -4,16 +4,36 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+    // define columns
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
